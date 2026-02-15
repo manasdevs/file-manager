1ef569d79563334ea491f0fcb0ef00b6d939e598
MAnasLatif <104300600+MAnasLatif@users.noreply.github.com>
2026-02-16 00:04:57 +0500

    chore(manas-fm): add eslint config and npmignore

---
+++ packages/manas-fm/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "no-console": "off",
+  },
+  ignorePatterns: ["dist/", "node_modules/", "*.js", "*.cjs"],
+};
